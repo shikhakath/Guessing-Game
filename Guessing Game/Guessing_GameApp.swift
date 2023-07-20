ea978730e8754674659474b6d25dfaa203c554e2
Shikha Kathrani <shikhadventure@gmail.com>
2023-07-20 10:29:01 -0700

    Initial Commit

---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Shikha Kathrani on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
